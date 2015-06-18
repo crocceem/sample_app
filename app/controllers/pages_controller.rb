55823c3c4ef88a0c33d740aceb94d48d29fd8e83
Emmanuel Croccel <emmanuel.croccel@reseau.eseo.fr>
2015-06-18 16:48:36 +0200

    Fini avec les pages statiques

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Accueil"
   end
 
   def contact
+    @title = "Contact"
   end
+
+  def about
+    @title = "À Propos"
+  end
+
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe PagesController, type: :controller do
-
+  render_views
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+    end
+
+    it "devrait avoir le bon titre" do
+      get :home
+      response.should have_selector("title",
+                                    :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
     end
   end
 
@@ -16,4 +23,10 @@ RSpec.describe PagesController, type: :controller do
     end
   end
 
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
