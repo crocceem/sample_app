220a566bb5befc49da7166e46c7c330b7a5d3ba5
Emmanuel Croccel <emmanuel.croccel@reseau.eseo.fr>
2015-06-23 19:45:45 +0200

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623130641) do
-
+ActiveRecord::Schema.define(version: 20150623141707) do
 
   create_table "users", force: :cascade do |t|
     t.string   "nom"
