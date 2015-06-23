05fc725cad5e3e1615162b3130b3fbe0dba013c0
Emmanuel Croccel <emmanuel.croccel@reseau.eseo.fr>
2015-06-23 17:53:40 +0200

    Changement de vues avec bootstrap

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Unicorn as the app server
 # gem 'unicorn'
-
+gem 'bootstrap-sass', '3.2.0.0'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'users/new'
 
-  get 'users/new'
-
-  get 'users/new'
-=======
   resources :users
->>>>>>> modeling-users
   get 'signup' => 'users#new'
 
   root 'pages#home'
